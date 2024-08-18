11ba05c38c5193821fd60e89c50307324849d8f1
ujwal <ujwalsai2@gmail.com>
2024-08-19 02:36:40 +0530

    added deployment pipeline

---
+++ buildscripts/appcheck.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd ..
+source venv/bin/activate
+PORT=8000
+DURATION=120
+END_TIME=$((SECONDS + DURATION))
+
+while [ $SECONDS -lt $END_TIME ]; do
+    RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:$PORT)
+    if [ $? -eq 0 ]; then
+        echo "Service is running."
+        exit 0
+    else
+        echo "Service is not running."
+    fi
+    sleep 10  # Check every 10 seconds
+done
+
+exit 1  # If the service was not running during the entire duration
---
+++ buildscripts/appstop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ..
+source venv/bin/activate
+PID=$(ps aux | grep 'uvicorn app.main:app' | grep -v grep | awk '{print $2}')
+if [ -n "$PID" ]; then
+  kill $PID
+  echo "Stopped uvicorn process with PID $PID"
+else
+  echo "No uvicorn process found"
