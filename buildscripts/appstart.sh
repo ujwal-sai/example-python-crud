a6619685131118901dd4ef0c65b51a18a77fce1d
ujwal <ujwalsai2@gmail.com>
2024-08-19 16:43:56 +0530

    permissions change

--- buildscripts/appstart.sh
+++ buildscripts/appstart.sh
@@ -3,5 +3,5 @@ chmod ugo+w /home/ubuntu/uvicorn.log
 python3.12 -m venv /home/ubuntu/venv
 source /home/ubuntu/venv/bin/activate
 pip3.12 install -r /home/ubuntu/requirements.txt
-nohup uvicorn app.main:app --host localhost --port 8000 --reload > uvicorn.log 2>&1 &
+nohup uvicorn app.main:app --host localhost --port 8000 --reload > /dev/null 2>&1 &
 sleep 2
